ea947c3f431a959617a2e35f629d5335212a8a17
ryildiz <rohatyildiz@gmx.de>
2019-09-10 18:12:17 +0200

    formatting

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import sys
 import MPU
 sys.path.insert(1, './src')
 import IMU_calc
-import reference_angle 
+import reference_angle
+import PID_Controller
 
-for i in range(0, 100):
-    ref_angle = reference_angle.reference_angle()
-    IMU_dynamic = MPU.MPU_9150(0, 1)
-    x_dynamic, y_dynamic, z_dynamic = IMU_dynamic.get_acceleration()
-    vec_dynamic = IMU_dynamic.get_acceleration()
-    IMU_static = MPU.MPU_9150(0, 0)
-    x_static, y_static, z_static = IMU_static.get_acceleration()
-    vec_static = IMU_static.get_acceleration()
-    current_angle = IMU_calc.calc_angle(vec_dynamic, vec_static)
-    print(current_angle)
-    error = ref_angle-current_angle
-    print(error)
+ref_angle = reference_angle.reference_angle()
+IMU_dynamic = MPU.MPU_9150(0, 1)
+vec_dynamic = IMU_dynamic.get_acceleration()
+IMU_static = MPU.MPU_9150(0, 0)
+vec_static = IMU_static.get_acceleration()
+current_angle = IMU_calc.calc_angle(vec_dynamic, vec_static)
+print(current_angle)
+error = ref_angle-current_angle
+print(error)
