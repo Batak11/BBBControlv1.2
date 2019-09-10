9e3b1dcbfa1492c2976352c4a2ec1c533705e1fb
ryildiz <rohatyildiz@gmx.de>
2019-09-10 15:43:04 +0200

    added reference_angle function

---
+++ src/reference_angle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 15:27:34 2019
+
+@author: rohatyildiz
+"""
+
+
+def reference_angle():
+    for i in range(0, 100):
+        ref_angle = int(input("What Reference Angle Would You like? "))
+        if ref_angle < 0:
+            ref_angle = 0
+        elif ref_angle > 180:
+            ref_angle = 180
+        print(ref_angle)
+        return(ref_angle)
