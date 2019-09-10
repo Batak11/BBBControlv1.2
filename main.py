f42969c5ea89d6a3fe23328704569cd9966ac0ed
ryildiz <rohatyildiz@gmx.de>
2019-09-10 17:49:46 +0200

    added for in loop to main and added PID Controller

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import MPU
 sys.path.insert(1, './src')
 import IMU_calc
-import reference_angle
+import reference_angle 
 
 IMU_dynamic = MPU.MPU_9150(0, 1)
 x_dynamic, y_dynamic, z_dynamic = IMU_dynamic.get_acceleration()
@@ -13,6 +13,10 @@ IMU_static = MPU.MPU_9150(0, 0)
 x_static, y_static, z_static = IMU_static.get_acceleration()
 vec_static = IMU_static.get_acceleration()
 
-current_angle = IMU_calc.calc_angle(vec_dynamic, vec_static)
-print(current_angle)
-ref_angle = reference_angle.reference_angle()
+for i in range(0, 100):
+    current_angle = IMU_calc.calc_angle(vec_dynamic, vec_static)
+    print(current_angle)
+    ref_angle = reference_angle.reference_angle()
+    print(ref_angle)
+    error = ref_angle-current_angle
+    print(error)
