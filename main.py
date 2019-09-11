8ad5e7b8713824220ff7c3e07570e8052e62bd57
ryildiz <rohatyildiz@gmx.de>
2019-09-11 13:51:30 +0200

    added cut and mapping function to math.py and extended Main for PWM signal

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import numpy
 import sys
 import MPU
+import Adafruit_BBIO.PWM as PWM
 sys.path.insert(1, './src')
 import IMU_calc
 import math
 import PID_Controller 
 
 PID_object = PID_Controller.PID(0, 0, 0)
+PID_object.sample_time(10)
+myPWM = "P8_13"
+PWM.start(myPWM, 0, 100000)
 
 
-for i in range(0, 100):
+for i in range(0, 20):
     print("Do you want to change Gains? if yes press y: ")
     if input() == "y":
         new_Kp = input()
@@ -31,6 +35,9 @@ for i in range(0, 100):
             vec_static = IMU_static.get_acceleration()
             current_angle = IMU_calc.calc_angle(vec_dynamic, vec_static)
             output = PID_object.update(current_angle)
+            cut_output = math.output_cut(output)
+            mapped_output = math.output_mapped(cut_output)
+            PWM.set_duty_cycle(myPWM, mapped_output)
 
     except KeyboardInterrupt:
         pass
--- src/math.py
+++ src/math.py
@@ -15,3 +15,16 @@ def reference_angle():
         ref_angle = 180
     print(ref_angle)
     return(ref_angle)
+
+
+def output_cut(output):
+    if output > 1:
+        output = 1
+    elif output < 0:
+        output = 0
+    return output
+
+
+def output_mapped(output_cut):
+    mapped_output = 100*output_cut
+    return mapped_output
