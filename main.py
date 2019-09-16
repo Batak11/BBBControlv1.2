4c91271358a345e6241e479b985c686874685e66
ryildiz <rohatyildiz@gmx.de>
2019-09-16 15:54:45 +0200

    changed gains

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import PID as PIDlib
 
 
 PID_object = PID_Controller.PID(0, 0, 0)
-PID = PIDlib.PidController([12, 1, 0.2], 0.2, 200)
+PID = PIDlib.PidController([1.2, .1, 0.02], 0.2, 200)
 
 
 myPWM = "P8_13"
