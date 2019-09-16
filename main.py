e8c9e65d30f81f88b174575e226e8ff595c3f19f
ryildiz <rohatyildiz@gmx.de>
2019-09-16 14:32:00 +0200

    added log function to main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from time import sleep
 import sys
 import MPU
@@ -15,32 +15,31 @@ IMU_static = MPU.MPU_9150(0, 0)
 IMU_dynamic = MPU.MPU_9150(0, 1)
 
 
-for i in range(0, 50):
-    # print("Do you want to change Gains? if yes press y: ")
-    # if input() == "y":
-    #    new_Kp = float(input())
-    #    PID_object.setKp(new_Kp)
-    #    new_Ki = float(input())
-    #    PID_object.setKi(new_Ki)
-    #    new_Kd = float(input())
-    #    PID_object.setKd(new_Kd)
-    # ref_angle = calc_functions.reference_angle()
-    DC = calc_functions.reference_angle()
-    # PID_object.setSetPoint(ref_angle)
-    # PID_object.setWindup(20)
+for i in range(0, 10):
+    print("Do you want to change Gains? if yes press y: ")
+    if input() == "y":
+        new_Kp = float(input())
+        PID_object.setKp(new_Kp)
+        new_Ki = float(input())
+        PID_object.setKi(new_Ki)
+        new_Kd = float(input())
+        PID_object.setKd(new_Kd)
+    ref_angle = calc_functions.reference_angle()
+    DC = 28.266*np.log(ref_angle)-55.595
+    PID_object.setSetPoint(ref_angle)
+    PID_object.setWindup(20)
     # PID_object.setSampleTime(0.3)
     try:
         while True:
             try:
-                PWM.set_duty_cycle(myPWM, DC)
                 vec_dynamic = IMU_dynamic.get_acceleration()
                 vec_static = IMU_static.get_acceleration()
                 current_angle = IMU_calc.calc_angle(vec_dynamic, vec_static)
-                # output = PID_object.update(current_angle)
+                output = PID_object.update(current_angle)
                 print(current_angle)
                 # cut_output = calc_functions.output_cut(output)
-                # PWM.set_duty_cycle(myPWM, cut_output)
-                
+                new_DC = DC+output
+                PWM.set_duty_cycle(myPWM, new_DC)
 
             except OSError:
                 pass
