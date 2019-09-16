044865a7dd3ba000367a0ea49a5de2d9ef5933c6
ryildiz <rohatyildiz@gmx.de>
2019-09-16 15:36:04 +0200

    changed Ki

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import PID as PIDlib
 
 
 PID_object = PID_Controller.PID(0, 0, 0)
-PID = PIDlib.PidController([0, 10, 0], 0.01, 99)
+PID = PIDlib.PidController([0.1, 10, 0], 0.01, 99)
 
 
 myPWM = "P8_13"
