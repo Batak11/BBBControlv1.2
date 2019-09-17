19dc289b817c293737d33a715107225cc7ee096a
ryildiz <rohatyildiz@gmx.de>
2019-09-17 15:16:17 +0200

    added better DC function to main

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ import calc_functions
 import PID_Controller 
 import PID as PIDlib 
 
-
-#PID_object = PID_Controller.PID(0, 0, 0)
+a = 1.463*10 ^ -9
+b = -8.223*10 ^ -7
+c = 0.0002
+d = -0.0252
+e = 2.1098
+f = -7.2276
+# PID_object = PID_Controller.PID(0, 0, 0)
 PID = PIDlib.PidController([0.02, .5, 0.02], 0.05, 50)
 
 
@@ -29,8 +34,8 @@ for i in range(0, 10):
 #        PID_object.setKi(new_Ki)
 #        new_Kd = float(input())
 #        PID_object.setKd(new_Kd)
-    DC = calc_functions.reference_angle()
-#    DC = 28.266*np.log(ref_angle)-55.595
+    ref_angle = calc_functions.reference_angle()
+    DC = a*ref_angle^5 + b*ref_angle^4 + c*ref_angle^3 + d*ref_angle^2 + e*ref_angle + f
 #    PID_object.setSetPoint(ref_angle)
 #    PID_object.setWindup(20)
 #    PID_object.setSampleTime(0.5)
@@ -41,18 +46,18 @@ for i in range(0, 10):
                 vec_static = IMU_static.get_acceleration()
                 current_angle = IMU_calc.calc_angle(vec_dynamic, vec_static)
 #                output = PID_object.update(current_angle)
-                #output_2 = PID.output(ref_angle, current_angle)
+                output_2 = PID.output(ref_angle, current_angle)
                 print('measured angle:\t', current_angle)
                 # cut_output = calc_functions.output_cut(output)
-                # new_DC = DC+output_2
-                # if new_DC > 99:
-                #    new_DC = 99
-                # elif new_DC < .01:
-                #    new_DC = .01
+                new_DC = DC+output_2
+                if new_DC > 99:
+                    new_DC = 99
+                elif new_DC < .01:
+                    new_DC = .01
 #                print('DC:\t\t', DC)
 #                print('output:\t\t', output)
-                # print('output2:\t\t', output_2, '\n')
-                PWM.set_duty_cycle(myPWM, DC)
+                print('output2:\t\t', output_2, '\n')
+                PWM.set_duty_cycle(myPWM, new_DC)
                 sleep(.05)
 
             except OSError:
