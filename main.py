42ef16847c43195203787410b649fce9c4419e61
ryildiz <rohatyildiz@gmx.de>
2019-09-24 16:26:32 +0200

    changed new_DC

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ c = 0.0002
 d = -0.0252
 e = 2.1098
 f = -7.2276
-# PID_object = PID_Controller.PID(0, 0, 0)
+
 PID = PIDlib.PidController([0.0, 1.0, 0.0], 0.05, 50)
 
 
@@ -26,37 +26,26 @@ IMU_dynamic = MPU.MPU_9150(0, 1)
 
 
 for i in range(0, 10):
-#    print("Do you want to change Gains? if yes press y: ")
-#    if input() == "y":
-#        new_Kp = float(input())
-#        PID_object.setKp(new_Kp)
-#        new_Ki = float(input())
-#        PID_object.setKi(new_Ki)
-#        new_Kd = float(input())
-#        PID_object.setKd(new_Kd)
+
     ref_angle = calc_functions.reference_angle()
     DC = a*(ref_angle**5) + b*(ref_angle**4) + c*(ref_angle**3) + d*(ref_angle**2) + e*ref_angle + f
-#    PID_object.setSetPoint(ref_angle)
-#    PID_object.setWindup(20)
-#    PID_object.setSampleTime(0.5)
+
     try:
         while True:
             try:
                 vec_dynamic = IMU_dynamic.get_acceleration()
                 vec_static = IMU_static.get_acceleration()
                 current_angle = IMU_calc.calc_angle(vec_dynamic, vec_static)
-#                output = PID_object.update(current_angle)
-                output_2 = PID.output(ref_angle, current_angle)
+                output = PID.output(ref_angle, current_angle)
                 print('measured angle:\t', current_angle)
-                # cut_output = calc_functions.output_cut(output)
-                new_DC = DC+output_2
+                new_DC = DC  # +output
                 if new_DC > 99:
                     new_DC = 99
                 elif new_DC < .01:
                     new_DC = .01
 #                print('DC:\t\t', DC)
 #                print('output:\t\t', output)
-                print('output2:\t\t', output_2, '\n')
+                print('output2:\t\t', output, '\n')
                 PWM.set_duty_cycle(myPWM, new_DC)
                 sleep(.05)
 
