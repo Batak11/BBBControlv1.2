6b3db384cfd3798bb40b854cf6341a3f0c1d48a9
ryildiz <rohatyildiz@gmx.de>
2019-10-01 19:02:50 +0200

    changed main

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ import PID as PIDlib
 # f = -21.65
 
 #Werte für 2 Bar
-a = -1.773E-7
-b = 7.232E-5
-c = -0.012
-d = 1.1541
-e = -0.0469
+# a = -1.773E-7
+# b = 7.232E-5
+# c = -0.012
+# d = 1.1541
+# e = -0.0469
 
 
 
@@ -36,8 +36,8 @@ IMU_dynamic = MPU.MPU_9150(0, 1)
 
 for i in range(0, 10):
 
-    ref_angle = calc_functions.reference_angle()
-    DC = a * (ref_angle ** 4) + b * (ref_angle ** 3) + c * (ref_angle ** 2) + d * ref_angle + e
+    DC = calc_functions.reference_angle()
+#    DC = a * (ref_angle ** 4) + b * (ref_angle ** 3) + c * (ref_angle ** 2) + d * ref_angle + e
 
     try:
         while True:
@@ -45,16 +45,16 @@ for i in range(0, 10):
                 vec_dynamic = IMU_dynamic.get_acceleration()
                 vec_static = IMU_static.get_acceleration()
                 current_angle = IMU_calc.calc_angle(vec_dynamic, vec_static)
-                output = PID.output(ref_angle, current_angle)
-                print(time.monotonic(), current_angle, ref_angle)
-#                print(current_angle)
-                new_DC = DC + output
-                if new_DC > 99.99:
-                    new_DC = 99.99
-                elif new_DC < .01:
-                    new_DC = .01
+#                output = PID.output(ref_angle, current_angle)
+#                print(time.monotonic(), current_angle, ref_angle)
+                print(current_angle)
+#                new_DC = DC + output
+#                if new_DC > 99.99:
+#                    new_DC = 99.99
+#                elif new_DC < .01:
+#                    new_DC = .01
 #                print('output:\t\t', output, '\n')
-                PWM.set_duty_cycle(myPWM, new_DC)
+                PWM.set_duty_cycle(myPWM, DC)
                 time.sleep(.01)
 
             except OSError:
