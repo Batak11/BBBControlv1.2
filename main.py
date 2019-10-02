0bbbe74e9e99488e7784643d4129906e60a5fe6b
ryildiz <rohatyildiz@gmx.de>
2019-10-02 16:12:53 +0200

    changed gainz

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ e = -0.0469
 
 
 
-PID = PIDlib.PidController([0.86, 4.5, 0.09], 0.1, 30)
+PID = PIDlib.PidController([0.86, 4.5, 0.09], 0.01, 30)
 
 
 myPWM = "P8_13"
@@ -61,7 +61,7 @@ for i in range(0, 10):
                     new_DC = .01
 #                print('output:\t\t', output, '\n')
                 PWM.set_duty_cycle(myPWM, new_DC)
-                time.sleep(.1)
+                time.sleep(.01)
 
             except OSError:
                 pass
